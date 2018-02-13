a51be47649abf60e47f0ec6f13f3be046e34be50
Tom Squillace <tomsquillace@gmail.com>
2018-02-13 21:34:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
   return "I can't hear you!"
   else if (string === string.toUpperCase())
-  return "YES, INDEED!"
+  return "YES INDEED!"
   else (string === "I love you, Grandma")
   return "I love you, too."
 }
