7fe462402c915e40d70df018f9fc4f4efe2fb6b6
Tom Squillace <tomsquillace@gmail.com>
2018-02-13 21:03:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
   
 }
 function sayHiToGradma(string) {
-  if string === string.toLowerCase() // true
+  if (string === string.toLowerCase())
   return "I can't hear you!"
 }
